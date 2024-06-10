bf5042bf9af94d7c453440a8eb039ebd40dd6b2c
gilbertsibuea <gilbertsibuea8539@gmail.com>
2024-06-10 15:08:35 +0700

    Edit Process #2

--- auth/login_admin.php
+++ auth/login_admin.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Customer</title>
+    <title>Login Admin</title>
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <script src="../assets/js/jquery.js"></script>
     <script src="../assets/js/popper.js"></script>
@@ -11,27 +11,23 @@
   </head>
   <body>
     <div class="container">
-      <!-- start-card -->
       <div class="mx-auto col-sm-6 mt-5">
         <p class="text-center" href="#" style="font-size: 250%;">Toko Buku</p>
         <div class="card">
-          <div class="card-header bg-success text-light text-center">
-            <h4>Login Customer</h4>
+          <div class="card-header bg-primary text-light text-center">
+            <h4>Login Admin</h4>
           </div>
           <div class="card-body">
-            <form action="cek_login_customer.php" method="post">
+            <form action="cek_login_admin.php" method="post">
               Username
               <input type="text" name="username" class="form-control" placeholder="Username" required>
               Password
               <input type="password" name="password" class="form-control" placeholder="Password" required>
-              <button type="submit" name="login_customer" class="btn btn-block btn-success mt-3 col-sm-4 mx-auto">Login</button>
+              <button type="submit" name="login_admin" class="btn btn-block btn-primary mt-3 col-sm-4 mx-auto">Login</button>
             </form>
-          </div>
-          <div class="card-footer text-center">
           </div>
         </div>
       </div>
-      <!-- end-card -->
     </div>
   </body>
 </html>
